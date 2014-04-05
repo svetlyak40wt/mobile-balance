c638f6f1b21dc9bc150629f4e1b2f1be947e466d
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-04-05 12:30:12 +0400

    Argument --number was renamed to --phone

---
+++ README.md
@@ -0,0 +1,23 @@
+Mobile Balance
+==============
+
+Installation
+------------
+
+Do as usual: `pip install mobile-balance`
+
+Usage
+-----
+
+Specify mobile operator and credential from it's personal profile.
+For example:
+
+    $ mobile-balance mts --phone=9154041111 --password=to-secret-to-share
+    196.513792
+
+Credits
+-------
+
+* Alexander Artemenko — for initial release.
+
+Want to help? Send pull request!
--- mobile_balance/main.py
+++ mobile_balance/main.py
@@ -3,14 +3,14 @@
 """Mobile Balance.
 
 Usage:
-  mobile-balance <operator> --number=<phone-number> --password=<password>
+  mobile-balance <operator> --phone=<phone-number> --password=<password>
   mobile-balance (-h | --help)
   mobile-balance --version
 
 Options:
   -h --help                Show this screen.
   --version                Show version.
-  --number=<phone-number>  Mobile number without 8 or +7 prefix.
+  --phone=<phone-number>  Mobile number without 8 or +7 prefix.
   --password=<password>    Password from personal profile at mobile operator's site.
 
 """
@@ -22,7 +22,7 @@ OPERATORS = dict(
     mts=mts)
 
 def main():
-    arguments = docopt(__doc__, version='Mobile Balance 0.1.0')
+    arguments = docopt(__doc__, version='Mobile Balance 0.1.1')
     mobile_operator = OPERATORS[arguments['<operator>']]
-    print mobile_operator.get_balance(arguments['--number'],
+    print mobile_operator.get_balance(arguments['--phone'],
                                       arguments['--password'])
