a36799a2ccf06016b1400c7ca8d5a89e7a2149b5
drnextgis <rykovd@gmail.com>
2016-04-19 12:46:56 +0700

    Fix get_balance for Tele2

--- mobile_balance/tele2.py
+++ mobile_balance/tele2.py
@@ -10,26 +10,27 @@ from .utils import check_status_code
 def get_balance(number, password):
     s = requests.Session()
 
-    response = s.post('https://login.tele2.ru:443/ssotele2/wap/auth/submitLoginAndPassword',
-                      data={'pNumber': number, 'password': password})
+    response = s.get('https://login.tele2.ru/ssotele2/wap/auth/')
     check_status_code(response, 200)
 
-    response = s.get('https://my.tele2.ru/home')
-    check_status_code(response, 200)
-
-    match = re.search(r"csrfTok(.*?): '(.*?)'", response.content)
-    if match is None:
+    match = re.search(r'value="(.*?)" name="_csrf"', response.content)
+    csrf_token = match.group(1)
+    if csrf_token is None:
         raise BadResponse('CSRF token not found', response)
 
-    csrf_field, csrf_value = match.groups()
-    csrf_field = 'csrfTok%s' % csrf_field
+    data = dict(pNumber=number, password=password, _csrf=csrf_token)
+    response = s.post(
+        'https://login.tele2.ru/ssotele2/wap/auth/submitLoginAndPassword',
+        data=data)
+    check_status_code(response, 200)
 
-    response = s.post('https://my.tele2.ru/balance/json',
-                      data={csrf_field: csrf_value, 'isBalanceRefresh': 'true'})
+    response = s.get('https://my.tele2.ru/main/tariffAndBalance')
     check_status_code(response, 200)
 
     balance = response.json().get('balance')
-    if balance is None:
-        raise BadResponse('Unable to get balance from JSON', response)
+    if balance is not None:
+        amount = balance.get('amount')
+    if balance is None or amount is None:
+        raise BadResponse('Unable to get balance amount from JSON', response)
 
-    return float(balance.split()[0].replace(',', '.'))
+    return float(amount.split()[0].replace(',', '.'))
