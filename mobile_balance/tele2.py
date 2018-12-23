32b278d08d94e19464a1c39c9620cdebb6364ee5
Alex McGunn <alexm@oo2.be>
2018-12-22 21:35:25 -0500

    fixed tele2 balance checker added basic phone number validation&cleanup

--- mobile_balance/tele2.py
+++ mobile_balance/tele2.py
@@ -4,11 +4,14 @@ import requests
 import re
 
 from .exceptions import BadResponse
-from .utils import check_status_code
+from .utils import check_status_code, cleanup_phone_number
 
 
 def get_balance(number, password):
     s = requests.Session()
+    # API expects phone number in strict international format
+    # Only digits: <COUNTRY_CODE><ABC/DEF><PHONE_NUMBER>
+    number = cleanup_phone_number(number)
 
     response = s.get('https://login.tele2.ru/ssotele2/wap/auth/')
     check_status_code(response, 200)
@@ -18,19 +21,17 @@ def get_balance(number, password):
     if csrf_token is None:
         raise BadResponse('CSRF token not found', response)
 
-    data = dict(pNumber=number, password=password, _csrf=csrf_token)
+    data = dict(pNumber=number, password=password, _csrf=csrf_token, authBy='BY_PASS', rememberMe='true')
     response = s.post(
         'https://login.tele2.ru/ssotele2/wap/auth/submitLoginAndPassword',
         data=data)
     check_status_code(response, 200)
 
-    response = s.get('https://my.tele2.ru/main/tariffAndBalance')
+    response = s.get('https://my.tele2.ru/api/subscribers/{}/balance'.format(number))
     check_status_code(response, 200)
 
-    balance = response.json().get('balance')
-    if balance is not None:
-        amount = balance.get('amount')
-    if balance is None or amount is None:
+    amount = response.json().get('data', {}).get('value', None)
+    if amount is None:
         raise BadResponse('Unable to get balance amount from JSON', response)
 
-    return float(amount.split()[0].replace(',', '.'))
+    return amount
