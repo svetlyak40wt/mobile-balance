2e9b376b0f8d560629339f1ab7968c571d8b113f
amnesia-victim <amnesia.victim@gmail.com>
2021-06-30 18:07:07 +0300

    Fixed Megafon CAPTCHA exception. Fixed Megafon LK login

--- mobile_balance/megafon.py
+++ mobile_balance/megafon.py
@@ -9,24 +9,35 @@ from .utils import check_status_code
 
 def get_balance(number, password):
     s = requests.Session()
+    try:
+        response = s.get('https://lk.megafon.ru/login/')
+        check_status_code(response, 200)
+    except:
+        print('Unable to get the data from Megafon site. Exiting!')
+        exit(1)
+    
+        # Replaced the cookie search
+    try:
+        csrf_token = response.cookies['CSRF-TOKEN']
+    except:
+        print('Unable to get the Megafon auth token. Exiting!')
+        exit(1)
 
-    response = s.get('https://lk.megafon.ru/login/')
-    check_status_code(response, 200)
-
-    csrf_token = re.search(r'name="CSRF" value="(.*?)"', response.content)
 
     if csrf_token is None:
         raise BadResponse('CSRF token not found', response)
-
-    csrf_token = csrf_token.group(1)
-
-    response = s.post('https://lk.megafon.ru/dologin/',
-                      data={'j_username': number,
-                            'j_password': password,
-                            'CSRF': csrf_token,
-                        })
-    check_status_code(response, 200)
-
+    try:
+        response = s.post('https://lk.megafon.ru/dologin/',
+                        data={'j_username': number,
+                                'j_password': password,
+                                'CSRF': csrf_token,
+                            })
+        check_status_code(response, 200)
+    except:
+        print('Unable to login the Megafon site. Exiting!')
+    
+    if u'Символы с картинки' in response.text:
+        raise BadResponse('CAPTCHA was shown', response)
     if u'Требуется ввод цифрового кода' in response.text:
         raise BadResponse('CAPTCHA was shown', response)
     if u'Как получить пароль' in response.text:
